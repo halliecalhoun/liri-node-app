dedef4ea776e87d9883b119ed28a2447bdfbb5d5
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-20 16:43:08 -0700

    spotifyThisSong() function working now, logging artist, song, preview url so far. Need to add album

--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
 
- 
 var userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(" ");
 
@@ -18,55 +17,56 @@ function userCommand(userInput, userQuery) {
     }
 }
 
+
 userCommand(userInput, userQuery);
 
 function spotifyThisSong() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+    console.log(`\n - - - - -\n\nSEARCHING FOR...`);
     // IF USER QUERY NOT FOUND, PASS VALUE OF "ACE OF BASE"
     if (!userQuery) {
-    userQuery = "I want it that way"
+    userQuery = 'All the Small Things'
     };
-    // SPOTIFY SEARCH QUERY FORMAT
     spotify.search({
     type: 'track',
     query: userQuery,
     limit: 1
     }, function (error, data) {
-    if (error) {
-    return console.log('Error occurred: ' + error);
-    }
+    
     // COLLECT SELECTED DATA IN AN ARRAY
-    var spotifyArr = data.tracks.items;
-    for (i = 0; i < spotifyArr.length; i++) {
-    console.log(`\nBA DA BOP! That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
-    };
-    });
-    }
-   
-    function doThis() {
-    // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
-    fs.readFile("random.txt", "utf8", function (error, data) {
+    // var spotifyArr = data.tracks.items;
+    // for (i = 0; i < spotifyArr.length; i++) {
+    // console.log(`\nBA DA BOP! That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name}
     if (error) {
-    return console.log(error);
+        return console.log('Error occurred: ' + error);
+        }
+        var tracksArr = data.tracks.items;
+    for (var i = 0; i < tracksArr.length; i++) {
+        // var songData = data.tracks.items[i];
+        console.log("Artist: " + data.tracks.items[i].artists[0].name);
+        console.log("Song: " + data.tracks.items[i].name);
+        console.log("Preview URL " + data.tracks.items[i].preview_url);
+
+        fs.appendFile('random.txt', data.tracks.items[i].artists[0].name);
+        fs.appendFile('random.txt', data.tracks.items[i].name);
+        fs.appendFile('random.txt', data.tracks.items[i].preview_url);
+    } 
+// } else {
+//     console.log('Error occurred: ' + error);
+// }
     }
-    // CATCH DATA AND USE THE .SPLIT() METHOD TO SEPARATE OBJECTS WITHIN OUR NEW ARRAY
-    let dataArr = data.split(",");
-    // TAKE OBJECTS FROM RANDOM.TXT TO PASS AS PARAMETERS
-    userInput = dataArr[0];
-    userQuery = dataArr[1];
-    // CALL OUR FUNCTION WITH OUR NEW PARAMETERS...
-    userCommand(userInput, userQuery);
-    });
-    };
+    )};
+    spotifyThisSong();
+   
+   
     
 
-spotify.search({ type: 'track', query: 'I want it that way' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
+// spotify.search({ type: 'track', query: 'I want it that way' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
    
-  console.log(data); 
-  });
+//   console.log(data); 
+//   });
  
 // spotify
 //   .search({ type: 'track', query: 'All the Small Things' })
@@ -75,4 +75,35 @@ spotify.search({ type: 'track', query: 'I want it that way' }, function(err, dat
 //   })
 //   .catch(function(err) {
 //     console.log(err);
-//   });
+//   });  
+
+
+// 
+// if (error) {
+//     return console.log('Error occurred: ' + error);
+//     }
+//     // COLLECT SELECTED DATA IN AN ARRAY
+//     var spotifyArr = data.tracks.items;
+//     for (i = 0; i < spotifyArr.length; i++) {
+//     console.log(`\nBA DA BOP! That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
+//     };
+//     });
+//     }
+   
+
+    // spotify.search({ type: 'track', query: 'I want it that way' }, function(err, data) {
+    //     if (err) {
+    //       return console.log('Error occurred: ' + err);
+    //     }
+       
+    //   console.log(data); 
+    //   });
+     
+    // spotify
+    //   .search({ type: 'track', query: 'All the Small Things' })
+    //   .then(function(response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function(err) {
+    //     console.log(err);
+    //   });
