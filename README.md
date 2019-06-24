fde5d7e5e6a1f7650379d6d7de14a23018b79e85
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-24 01:01:25 -0700

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# liri-node-app
+# Liri Node App
+
+## Description
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+LIRI will use API's to search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+## Command List
+### LIRI is a command line node app that takes the four commands below:
+* concert-this
+* spotify-this-song
+* movie-this
+* do-what-it-says
+
+## Concert-this:
+
+### Node Command Code:
+```
+node liri.js spotify-this-song <'song name here'> 
+```
+![Concert-this Command Screenshot](assets\images\concert.PNG)
+
+* node liri.js concert-this <'artist/band name here'>
+
+![Concert-this Command Screenshot](assets\images\concert.PNG)
+
+## Spotify-this-song:
+
+### Node Command Code with Defined Song:
+```
+node liri.js spotify-this-song <'song name here'>
+```
+![Concert-this Command Screenshot](assets\images\spotify-this-song.PNG)
+
+### Node Command Code with Undefined Song:
+If there is no song provided, then the program will default to "The Sign" by Ace of Base, as seen in the example below:
+
+```
+node liri.js spotify-this-song
+```
+![Spotify-this-song Undefined](assets\images\ace.PNG)
+
+## Movie-this:
+
+### Node Command Code with Defined Movie:
+```
+node liri.js movie-this <'movie name here'>
+```
+![Concert-this Command Screenshot](assets\images\movie-this.PNG)
+
+### Node Command Code with Undefined Movie:
+If there is no movie provided, then the program will default to the movie "Mr. Nobody," as seen in the example below:
+
+```
+node liri.js movie-this
+```
+![Spotify-this-song Undefined](assets\images\mr-nobody.PNG)
+
+## Built With
+* JavaScript- Code executed within Node.js
+* Node.js- JavaScript runtime environment
+* Node Packages:
+    * Node Spotify API- For spotify-this-song command
+    * Axios- For calling Bands in Town & OMDB APIs
+    * Moment- Used for Date Formatting
+* Bands in Town API- For concert-this command
+* OMDB API- For movie-this command
+
+## Author
+Hallie Calhoun
--- liri.js
+++ liri.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 var request = require('request');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-
-// var fs = require("fs");
 var moment = require('moment');
 var axios = require('axios');
-
-
 var fs = require("fs");
 fs.appendFile('log.txt', userInput + ",", function (err) {
     if (err) throw err;
@@ -17,12 +13,8 @@ var userInput = process.argv;
 var userQuery = userInput[2];
 // var userQuery = process.argv.slice(3).join(" ");
 
-// var input = process.argv;
-// var command = input[2];
-
-//variable for holding the name of the movie, artist , or song
+//variable for holding input
 var name = "";
-
 for (var i = 3; i < userInput.length; i++) {
     if (i > 3 && i < userInput.length) {
         name = name + "+" + userInput[i];
@@ -36,7 +28,7 @@ switchCase();
 function switchCase() {
     switch (userQuery) {
         case "concert-this":
-            showConcertInfo(name);
+            concertThis(name);
             break;
 
         case "spotify-this-song":
@@ -47,8 +39,9 @@ function switchCase() {
             movieThis(name);
             break;
 
+        // need to practice switch case more to perform the function below:
         case 'do-what-it-says':
-            showSomeInfo();
+            showInfo();
             break;
 
         default:
@@ -57,121 +50,24 @@ function switchCase() {
     }
 }
 
-
-// function switchCase(userInput, userQuery) {
-//     switch (userInput) {
-
-//             case 'concert-this':
-//                 showConcertInfo();
-//                 break;
-
-//                 case 'spotify-this-song':
-//             spotifyThisSong();
-//             break;
-
-//                 case 'movie-this':
-//                     movieThis();
-//                     break;
-
-//             case 'do-what-it-says':
-//                     showSomeInfo();
-//                     break;
-
-//     }
-// }
-// switchCase(userInput, userQuery);
-// function userCommands(userInput, userQuery) {
-//     switch (userInput) {
-//         case "spotify-this":
-//         spotifyThisSong();
-//         break;
-//     }
-
-// case "do-what-it-says":
-//     doThis();
-//     break;
-// };
 var divider = "\n------------------------------------------------------------\n\n";
 
-// function showConcertInfo(artist) {
-//     console.log(`\n - - - - -\n\nSEARCHING FOR ARTIST...`);
-//     // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-//     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-//         function(response) {
-//             // console.log(response);
-//                 var concerts = response.data;
-//                 for (var i = 0; i < concerts.length; i++) {
-//                     if (concerts[i].venue != undefined) {
-//                     // var output = 
-//                     console.log("--------------------EVENT INFO--------------------")
-
-//                     console.log("Venue Name: " + concerts[0].venue.name);
-//                     console.log("Venue Location: " + concerts[0].venue.city);
-//                     // var eventDateAndTime = moment(concerts[i].datetime);
-//                     console.log("Event Date: " + moment(concerts[0].datetime).format("MM/DD/YYYY"));
-//                     logConcerts = "Venue name: " + concerts[0].venue.name + "\nVenue Location: " + concerts[0].venue.city +
-//         "\nEvent Date: " + moment(concerts[0].datetime).format("MM/DD/YYYY") + "\n";
-
-
-
-//                     }
-//                else {
-//                 console.log("No results found.");
-//             }
-
-//         }
-//         }
-//     ).catch(function (error) {
-//         console.log(error);
-//     })
-// fs.appendFile('log.txt', logConcerts + divider, function(error) {
-//     if (error) throw error;
-//     }) 
-// };
-
-
-
-function showConcertInfo(name) {
-    console.log(`\n - - - - -\n\nSEARCHING FOR ARTIST...`);
+function concertThis(name) {
+    console.log(`\n - - - - -\n\nSEARCHING FOR EVENT...`);
     // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get("https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp").then(
         function (response) {
-
             var concertsArr = response.data;
-            for (var i = 0; i < concertsArr.length; i++) {
-            if (concertsArr[i].venue != undefined) {
-            // console.log("Release Year: " + response.data.Year);
-            console.log("--------------------EVENT INFO--------------------");
-            console.log("Venue Name: " + concertsArr[i].venue.name);
-            console.log("Venue Location: " + concertsArr[i].venue.city);
-            // var eventDateAndTime = moment(concerts[i].datetime);
-            console.log("Event Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY"));
-
-            var logConcerts = "Venue Name: " + concertsArr[i].venue.name + "\nVenue Location: " + concertsArr[i].venue.city + "\nEvent Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY") + "\n";
-        
+            // for (var i = 0; i < concertsArr.length; i++) {
+            if (concertsArr[i] != undefined) {
+                console.log("--------------------EVENT INFO--------------------");
+                var logConcerts = "Venue Name: " + concertsArr[i].venue.name + "\nVenue Location: " + concertsArr[i].venue.city + "\nEvent Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY") + "\n";
             }
-
-
-            var concertsArr = response.data;
-            for (var i = 0; i < concertsArr.length; i++) {
-                if (concertsArr[i].venue != undefined) {
-                    console.log("--------------------EVENT INFO--------------------");
-                    console.log("Venue Name: " + concertsArr[i].venue.name);
-                    console.log("Venue Location: " + concertsArr[i].venue.city);
-                    // var eventDateAndTime = moment(concerts[i].datetime);
-                    console.log("Event Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY"));
-
-                    var logConcerts = "Venue Name: " + concertsArr[i].venue.name + "\nVenue Location: " + concertsArr[i].venue.city + "\nEvent Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY") + "\n";
-                    fs.appendFile('log.txt', logConcerts + divider, function (error) {
-                        if (error) throw error;
-                        writeToLog(logTracks);
-                    });
-                }
-            }
-        })
-
-
-        .catch(function (error) {
+            fs.appendFile('log.txt', logConcerts + divider, function (error) {
+                if (error) throw error;
+                console.log(logConcerts);
+            });
+        }).catch(function (error) {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
@@ -193,54 +89,15 @@ function showConcertInfo(name) {
         });
 }
 
-// fs.appendFile('log.txt', logConcerts + divider, function(error) {
-//     if (error) throw error;
-//     }) 
-
-
-// showConcertInfo();
-
-
-// var concertsArr = response.data;
-// for (var i = 0; i < concertsArr.length; i++) {
-// if (concertsArr[i].venue.name != undefined) {
-// console.log("Release Year: " + response.data.Year);
-// console.log("--------------------EVENT INFO--------------------");
-// console.log("Venue Name: " + concertsArr[i].venue.name);
-// console.log("Venue Location: " + concertsArr[i].venue.city);
-// var eventDateAndTime = moment(concerts[i].datetime);
-// console.log("Event Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY"));
-
-// var logConcerts = "Venue Name: " + concertsArr[i].venue.name + "\nVenue Location: " + concertsArr[i].venue.city + "\nEvent Date: " + moment(concertsArr[i].datetime).format("MM/DD/YYYY") + "\n";
-// fs.appendFile('log.txt', logConcerts + divider, function(error) {
-//     if (error) throw error;
-// writeToLog(logTracks);
-// });
-// }
-
-
-
-// userCommand(userInput, userQuery);
 var divider = "\n------------------------------------------------------------\n\n";
-// var spotifyArray = [];
-// function spotifyThisSong(song) {
-//     console.log(`\n - - - - -\n\nSEARCHING FOR...`);
-// spotify
-//   .search({ type: 'track', query: 'song' })
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+
 function spotifyThisSong(name) {
-    console.log(`\n - - - - -\n\nSEARCHING FOR...`);
+    console.log(`\n - - - - -\n\nSEARCHING FOR SONG...`);
     // var isInputNull = userQuery === "" ? userQuery = "The Sign Ace of Base" : userQuery = userQuery;
     if (!name) {
         name = "The Sign Ace of Base";
     }
     var spotify = new Spotify(keys.spotify);
-
     spotify.search({
         type: "track",
         query: name,
@@ -249,90 +106,40 @@ function spotifyThisSong(name) {
         if (err) {
             return console.log(err);
         } else {
-            console.log("Artist: " + data.tracks.items[0].album.artists[0].name); // artist's name
-            console.log("Song name: " + data.tracks.items[0].name) // song name
-            console.log("External url: " + data.tracks.items[0].album.external_urls.spotify) // external link
-            console.log("Album: " + data.tracks.items[0].album.name) // album name
+            console.log("--------------------SONG INFO--------------------");
+            console.log("\nSong: " + data.tracks.items[0].name +
+            "\nArtist: " + data.tracks.items[0].album.artists[0].name +
+                "\nSpotify Link: " + data.tracks.items[0].album.external_urls.spotify +
+                "\nAlbum: " + data.tracks.items[0].album.name + "\n");
         }
         fs.appendFile("log.txt", "\nAppending this song and artist data: " +
             "\n" + data.tracks.items[0].album.artists[0].name +
             "\n" + data.tracks.items[0].name +
             "\n" + data.tracks.items[0].album.external_urls.spotify +
-            "\n" + data.tracks.items[0].album.name, function (err) {
+            "\n" + data.tracks.items[0].album.name + "\n", function (err) {
                 if (err) {
                     console.log(err);
                 }
             })
     })
 }
-// spotify.search({ type: 'track', query: 'song' }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-
-//   console.log(data); 
-//   })
-// };
-
-// function spotifyThisSong(song) {
-//     console.log(`\n - - - - -\n\nSEARCHING FOR...`);
-//     spotify.search({
-//     type: 'track',
-//     query: song,
-//     limit: 1
-//     }, function (error, data) {
-//         console.log();
-//     if (error) {
-//         return console.log('Error occurred: ' + error);
-//         };
-//         if (!song) {
-//             song = "The Sign Ace of Base";
-//             } else {
-//         var tracksArr = data.tracks.items;
-//         for (var i = 0; i < tracksArr.length; i++) {
-//         if (!error) {
-//         console.log("--------------------SPOTIFY INFO--------------------");
-// var songData = data.tracks.items[i];
-// var showData = [
-// console.log("Artist: " + tracksArr[i].artists[i].name);
-// console.log("Song: " + tracksArr[i].name);
-// console.log("Album: " + data.tracks.items[i].album.name);
-// data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify
-//     console.log("Spotify link: " + data.tracks.items[i].external_urls.spotify);
-//     var logTracks = "Artist: " + data.tracks.items[i].artists[0].name + "\nSong name: " + data.tracks.items[i].name +
-//     "\nAlbum Name: " + data.tracks.items[i].album.name + "\nExternal Link: " + data.tracks.items[i].external_urls.spotify + "\n";
-//     fs.appendFile('log.txt', logTracks + divider, function(error) {
-//         if (error) throw error;
-//     });
-//     }
-//     }
-// }
-// }
-// )};
-
-
-// spotifyThisSong();
 
 var divider = "\n------------------------------------------------------------\n\n";
 
 
 function movieThis(name) {
-    console.log(`\n - - - - -\n\nSEARCHING FOR MOVIE...`);
     //    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
     axios.get("http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             console.log()
-            // console.log(response);
-            // console.log(response.data.Title);
             var movieData = response.data;
             // for (var i = 0; i < movieData.length; i++) {
             // console.log(movieData[i].Title);
             if (name === "") {
-                name = "Mr. Nobody";
-                console.log("Movie Title: Mr. Nobody");
+                movieThis("Mr. Nobody");
             } else {
                 // if (movieData.Title != undefined) {
+                console.log(`\n - - - - -\n\nSEARCHING FOR MOVIE...`);
                 console.log("--------------------MOVIE INFO--------------------");
                 console.log("Title: " + movieData.Title);
                 console.log("Year: " + movieData.Year);
@@ -347,13 +154,8 @@ function movieThis(name) {
                     "\nYear: " + movieData.Year + "\nIMDB Rating: " + movieData.imdbRating + "\nRotten Tomatoes Rating: " + movieData.tomatoRating + "\nCountry: " + movieData.country + "\nLanguage: " + movieData.Language + "\nPlot: " + movieData.Plot + "\nActors: " + movieData.Actors + "\n";
                 fs.appendFile('log.txt', logMovies + divider, function (error) {
                     if (error) throw error;
-                    // writeToLog(logTracks);
                 });
             }
-            // } else {
-            //     movieThis("Mr. Nobody");
-            // }
-
         }).catch(function (error) {
             if (error.response) {
                 // The request was made and the server responded with a status code
@@ -376,45 +178,29 @@ function movieThis(name) {
         })
 
 };
-// movieThis();
 
-
-// function doThis() {
-// // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
-// fs.readFile("random.txt", "utf8", function (error, data) {
-// if (error) {
-// return console.log(error);
-// }
-// // CATCH DATA AND USE THE .SPLIT() METHOD TO SEPARATE OBJECTS WITHIN OUR NEW ARRAY
-// let dataArr = data.split(",");
-// // TAKE OBJECTS FROM RANDOM.TXT TO PASS AS PARAMETERS
-// userInput = dataArr[0];
-// userQuery = dataArr[1];
-// // CALL OUR FUNCTION WITH OUR NEW PARAMETERS...
-// // userCommand(userInput, userQuery);
-// });
-// };
-
+// need to practice switch case more to perform the function below
 function showInfo() {
     fs.readFile('random.txt', 'utf8', function (error, data) {
         if (error) {
             return console.log(err);
         }
         var dataArr = data.split(',');
-        userInput = dataArr[0];
-        userQuery = dataArr[1];
-        // switchCase(dataArr[0], dataArr[1]);
+        var userInput = dataArr[0];
+        var userQuery = dataArr[1];
+        // switchCase(userInput, userQuery);
+        spotifyThisSong(userQuery);
     });
 };
 function showSomeInfo() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         var dataArr = data.split(",");
-        spotifyThisSong(dataArr[1])
+        var name = dataArr[1];
+        spotifyThisSong(name);
+        
         // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
     });
 };
-
-    // switchCase();
